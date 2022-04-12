103bcee8b3fa88b719b6555485c1e9a7d837cbee
tienle1996 <47994792+tienle1996@users.noreply.github.com>
2022-04-11 19:17:04 -0500

    make it bette

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,7 @@ require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
